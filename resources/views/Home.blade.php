358dee0a86c5e3dabc3338cbaf2eca98daae03d8
Prasanna Deshappriya <prasanna.14@cse.mrt.ac.lk>
2017-03-14 18:31:13 +0530

    Improve home page details

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -1,3 +1,19 @@
 @extends('layout.FrontLayout')
 
-
+@section('body')
+    <div class="container">
+        <div class="box">
+            <div class="row">
+                <div class="col-md-12">
+                    <img src="{{asset('img/myself.jpg')}}" width="280" height="300" class="img-circle img-left"/>
+                    <p>
+                        My name is Prasanna Deshappriya Bandara. I’m an undergraduate student from University of Moratuwa and currently specializing ICE (Integrated Computer Engineering) stream where we focus on not only the computer programming, but also hardware as well. I’m always curious about new technologies in the world. Since I’m a passionate programmer and a fast learner, I can try and learn them within short period of time.
+                        <br>This website is my personal website and it is maintain by me. Here you can get to know about my projects, my personal details, my skills and various other stuff. And most importantly, you are most welcome to leave a comment as well. Some of my projects are listed under project section. Under tutorial section, you can learn some basic programming stuffs. Also there you can learn to build libraries such as PCL(Point Cloud Library), OpenCV library and ect.
+                        <br>Furthermore, I have completed several projects for the past few years. Some of them are related to the academic modules (semester projects, etc. ) and some of them are not related to the university modules. I have published them in this website so that you can get ideas to your projects as well. Some of them are related to Image Processing and Point Cloud Processing, in which case you need to find out how to build those libraries before following those projects. My latest project is a mobile app called “Moodle”. This app allows to interact with the university moodle account easily. You can easily get to know about submissions and other academic related stuff without access to the moodle website. Furthermore, users can track their SGPA and GPA as well. Apart from that I have completed a robot tailor project. There users can connect multiple Kinects to same computer and use them simultaneously. They can take multiple frames and register them. Then show a 3d point cloud to examin.
+                        <br>Under the contact section you can contact me by ending an email. And my Facebook, Github, Twitter, Linkedin profiles are listed below so that you can contact me there as well. Also you can see some source codes of my projects in my github profile. Some of them may not public. If you need help with a code, please send me a email.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+@stop
