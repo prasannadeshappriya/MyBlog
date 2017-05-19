189397baebe15f2b6e798a8a9e50ead53ba2b9d8
Prasanna Deshappriya <prasanna.14@cse.mrt.ac.lk>
2017-05-19 19:59:04 +0530

    Deploy on herokuapp server

--- config/database.php
+++ config/database.php
@@ -1,9 +1,9 @@
 <?php
-//$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-//$host = $url["host"];
-//$username = $url["user"];
-//$password = $url["pass"];
-//$database = substr($url["path"], 1);
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
 return [
 
     /*
@@ -43,33 +43,33 @@ return [
             'prefix' => '',
         ],
 
-//        'mysql' => [
-//            'driver' => 'mysql',
-//            'host' => $host,
-//            'port' => env('DB_PORT', '3306'),
-//            'database' => $database,
-//            'username' => $username,
-//            'password' => $password,
-//            'charset' => 'utf8mb4',
-//            'collation' => 'utf8mb4_unicode_ci',
-//            'prefix' => '',
-//            'strict' => true,
-//            'engine' => null,
-//        ],
-
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => $host,
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'strict' => true,
             'engine' => null,
         ],
+
+//        'mysql' => [
+//            'driver' => 'mysql',
+//            'host' => env('DB_HOST', '127.0.0.1'),
+//            'port' => env('DB_PORT', '3306'),
+//            'database' => env('DB_DATABASE', 'forge'),
+//            'username' => env('DB_USERNAME', 'forge'),
+//            'password' => env('DB_PASSWORD', ''),
+//            'charset' => 'utf8mb4',
+//            'collation' => 'utf8mb4_unicode_ci',
+//            'prefix' => '',
+//            'strict' => true,
+//            'engine' => null,
+//        ],
 
         'pgsql' => [
             'driver' => 'pgsql',
